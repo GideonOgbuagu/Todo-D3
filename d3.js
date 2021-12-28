23ecf05056bac1572c686ba772fe8ebab80aa609
GideobOgbuagu <reachgiddy@gmail.com>
2021-12-28 09:30:20 -0500

    separated the js file as d3.js

---
+++ d3.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+  d3.select("#new-note").on("submit", function () {
+    d3.event.preventDefault();
+
+    let input = d3.select("input");
+    d3.select("#notes").append("p").classed("note", true).text(input.property("value"));
+    input.property("value", "")
+  });
+})
